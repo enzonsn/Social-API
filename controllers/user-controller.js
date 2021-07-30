7fd1ba7885d017b38dc099beb69acb91b2615225
enzonsn <enzonsperduti@gmail.com>
2021-07-30 04:21:42 -0500

    changed catch statements to arrow functions for code optimization

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,7 +6,7 @@ const userController = {
             .select('-__v')
             .populate({ path: 'thoughts', select: '-__v' })
             .then(dbUserData => res.json(dbUserData))
-            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+            .catch(err => {console.log(err); res.status(400).json(err);});
     },
     getUserById({params}, res){
         User.findOne({ _id: params.id })
@@ -15,17 +15,17 @@ const userController = {
             .then(dbUserData => {
                 if(!dbUserData){ res.status(404).json({message: 'No user found under provided id!'}); return;}
                 res.json(dbUserData);})
-            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+            .catch(err => {console.log(err); res.status(400).json(err);});
     },
     createUser({body}, res){
         User.create(body).then(dbUserData => res.json(dbUserData)).catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
     },
-    UpdateUser({params, body}, res){
+    updateUser({params, body}, res){
         User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
             .then(dbUserData => {
                 if(!dbUserData){ res.status(404).json({message: 'No user found under provided id!'}); return;}
                 res.json(dbUserData);})
-            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+            .catch(err => {console.log(err); res.status(400).json(err);});
     },
     deleteUser({params}, res){
         User.findOneAndDelete({ _id: params.id })
@@ -35,11 +35,11 @@ const userController = {
                     .then(() =>{
                         Thought.deleteMany({ username: dbUserData.username })
                             .then(() => { res.json({message: 'Succesfully deleted User!'}) })
-                            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+                            .catch(err => {console.log(err); res.status(400).json(err);});
                     })
-                    .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+                    .catch(err => {console.log(err); res.status(400).json(err);});
             })
-            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+            .catch(err => {console.log(err); res.status(400).json(err);});
     },
     addFriend({params}, res){
         User.findOneAndUpdate({_id: params.userId}, {$addToSet: { friends: params.friendId }}, {new: true, runValidators: true})
@@ -50,9 +50,9 @@ const userController = {
                         if(!dbUserData2){ res.status(404).json({message: 'No user found under provided id!'}); return;}
                         res.json(dbUserData);
                     })
-                    .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+                    .catch(err => {console.log(err); res.status(400).json(err);});
             })
-            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+            .catch(err => {console.log(err); res.status(400).json(err);});
     },
     deleteFriend({params}, res){
         User.findOneAndUpdate({_id: params.userId}, {$pull: { friends: params.userId }}, {new: true, runValidators: true})
@@ -62,9 +62,9 @@ const userController = {
                     .then(dbUserData => {
                         if(!dbUserData){ res.status(404).json({message: 'No user found under provided id!'}); return;}
                         res.json({message: 'Successfully deleted friend!'});})
-                    .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+                        .catch(err => {console.log(err); res.status(400).json(err);});
             })
-            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+            .catch(err => {console.log(err); res.status(400).json(err);});
     }
 }
 
