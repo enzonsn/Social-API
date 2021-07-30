843cd47849537d5c1b175aa9cafd4d2f6b50d12e
enzonsn <enzonsperduti@gmail.com>
2021-07-30 04:17:56 -0500

    added controllers addFriend and deleteFriend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -40,6 +40,31 @@ const userController = {
                     .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
             })
             .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+    },
+    addFriend({params}, res){
+        User.findOneAndUpdate({_id: params.userId}, {$addToSet: { friends: params.friendId }}, {new: true, runValidators: true})
+            .then(dbUserData => {
+                if(!dbUserData){ res.status(404).json({message: 'No user found under provided id!'}); return;}
+                User.findOneAndUpdate({_id: params.friendId}, {$addToSet: { friends: params.userId }}, {new: true, runValidators: true})
+                    .then(dbUserData2 => {
+                        if(!dbUserData2){ res.status(404).json({message: 'No user found under provided id!'}); return;}
+                        res.json(dbUserData);
+                    })
+                    .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+            })
+            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+    },
+    deleteFriend({params}, res){
+        User.findOneAndUpdate({_id: params.userId}, {$pull: { friends: params.userId }}, {new: true, runValidators: true})
+            .then(dbUserData => {
+                if(!dbUserData){ res.status(404).json({message: 'No user found under provided id!'}); return;}
+                User.findOneAndUpdate({_id: params.friendId}, {$pull: { friends: params.userId }}, {new: true, runValidators: true})
+                    .then(dbUserData => {
+                        if(!dbUserData){ res.status(404).json({message: 'No user found under provided id!'}); return;}
+                        res.json({message: 'Successfully deleted friend!'});})
+                    .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
+            })
+            .catch(function(err){ if(err) {console.log(err); res.status(400).json(err);}});
     }
 }
 
